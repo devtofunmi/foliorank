8be24a420a0fac5276de5a19069fe6269b9c7e94
devtofunmi <olayiwolajesutofunmi@gmail.com>
2025-06-11 22:03:17 +0100

    .

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  images: {
+    domains: ['api.dicebear.com'],
+  },
 };
 
 export default nextConfig;
